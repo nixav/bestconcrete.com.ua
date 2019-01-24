cb38a2bc9625b903c823020a7c8bc7c344762fd2
savoliukk <nikolay.savolyk@gmail.com>
2019-01-24 19:03:02 +0200

    Редирект корзины на форму заказа

--- wp-content/themes/tobi/functions.php
+++ wp-content/themes/tobi/functions.php
@@ -204,12 +204,47 @@ function tobi_pre_get_posts($query) {
 
 add_action('pre_get_posts', 'tobi_pre_get_posts');
 
-// ---------- Вывод характеристик в каталоге ----------
+/**
+ * Auto update cart after quantity change
+ *
+ * @return  string
+ **/
+add_action( 'wp_footer', 'cart_update_qty_script' );
+function cart_update_qty_script() {
+    if (is_cart()) :
+    ?>
+    <script>
+        jQuery('div.woocommerce').on('change', '.qty', function(){
+            jQuery("[name='update_cart']").trigger("click"); 
+        });
+    </script>
+    <?php
+    endif;
+};
 
-function my_template_loop_product_title(){
-    global $product;
-    $versionvalues = get_the_terms( $product->id, 'pa_width');
+function devvn_product_category_base_same_shop_base( $flash = false ){
+    $terms = get_terms(array(
+        'taxonomy' => 'product_cat',
+        'post_type' => 'product',
+        'hide_empty' => false,
+    ));
+    if ($terms && !is_wp_error($terms)) {
+        $siteurl = esc_url(home_url('/'));
+        foreach ($terms as $term) {
+            $term_slug = $term->slug;
+            $baseterm = str_replace($siteurl, '', get_term_link($term->term_id, 'product_cat'));
 
-    echo $versionvalues = get_the_terms( $product->id, 'pa_width');
+            add_rewrite_rule($baseterm . '?$','index.php?product_cat=' . $term_slug,'top');
+            add_rewrite_rule($baseterm . 'page/([0-9]{1,})/?$', 'index.php?product_cat=' . $term_slug . '&paged=$matches[1]','top');
+            add_rewrite_rule($baseterm . '(?:feed/)?(feed|rdf|rss|rss2|atom)/?$', 'index.php?product_cat=' . $term_slug . '&feed=$matches[1]','top');
 
+        }
+    }
+    if ($flash == true)
+        flush_rewrite_rules(false);
+}
+add_filter( 'init', 'devvn_product_category_base_same_shop_base');
+add_action( 'create_term', 'devvn_product_cat_same_shop_edit_success', 10, 2 );
+function devvn_product_cat_same_shop_edit_success( $term_id, $taxonomy ) {
+    devvn_product_category_base_same_shop_base(true);
 }
--- wp-content/themes/tobi/js/woocommerce.js
+++ wp-content/themes/tobi/js/woocommerce.js
@@ -1,26 +1,26 @@
-//jQuery(document).ready(function () {
-//    var $ = jQuery;
-//
-//
-//
-//    $(document).on('click', '.plus, .minus', function () {
-//        var $this = $(this);
-//        var qty_input = $this.parent().find('.qty');
-//        var current_qty = parseInt(qty_input.val());
-//        var add_to_cart_button = $this.parents('.add-to-cart-wrap').find('.ajax_add_to_cart')
-//
-//
-//
-//        if ($this.hasClass('plus')) {
-//            qty_input.val(current_qty + 1)
-//            $('.woocommerce-cart-form :input[name="update_cart"]').prop('disabled', false);
-//        } else if ($this.hasClass('minus')) {
-//            if (current_qty > 1) {
-//                qty_input.val(current_qty - 1)
-//                $('.woocommerce-cart-form :input[name="update_cart"]').prop('disabled', false);
-//            }
-//
-//            add_to_cart_button.attr('data-quantity', qty_input.val())
-//        }
-//    });
-//});
+jQuery(document).ready(function () {
+    var $ = jQuery;
+
+
+
+    $(document).on('click', '.plus, .minus', function () {
+        var $this = $(this);
+        var qty_input = $this.parent().find('.qty');
+        var current_qty = parseInt(qty_input.val());
+        var add_to_cart_button = $this.parents('.add-to-cart-wrap').find('.ajax_add_to_cart')
+
+
+
+        if ($this.hasClass('plus')) {
+            qty_input.val(current_qty + 1)
+            $('.woocommerce-cart-form :input[name="update_cart"]').prop('disabled', false);
+        } else if ($this.hasClass('minus')) {
+            if (current_qty > 1) {
+                qty_input.val(current_qty - 1)
+                $('.woocommerce-cart-form :input[name="update_cart"]').prop('disabled', false);
+            }
+
+            add_to_cart_button.attr('data-quantity', qty_input.val())
+        }
+    });
+});
